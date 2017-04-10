ebbeebd6e02adfa841bb84172e459bba084894f6
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2017-04-10 06:14:13 +0200

    upd

---
+++ googleart_scraper/linkextractor.py
@@ -0,0 +1,98 @@
+import re
+import six
+from six.moves.urllib.parse import urlparse, urljoin
+import json
+import lxml.etree as etree
+
+from scrapy.link import Link
+import scrapy.linkextractors
+import scrapy.linkextractors.lxmlhtml
+from scrapy.utils.response import get_base_url
+from scrapy.utils.python import unique as unique_list, to_native_str
+from scrapy.utils.misc import arg_to_iter
+
+
+_collect_string_content = etree.XPath("string()")
+_re_type = type(re.compile("", 0))
+
+
+class ParserLinkExtractor(scrapy.linkextractors.lxmlhtml.LxmlParserLinkExtractor):
+    """
+    Class to extracting links from tag <script type="text/javascript">...</script>.
+    This links are stored in javascript array, not in html tags.
+    The class extracts links from the body of the tag using list of regular expressions `url_regexps`.
+    """
+
+    def __init__(self, url_regexps, process=None, unique=False):
+        self.url_regexps = url_regexps
+        self.process_attr = process if callable(process) else lambda v: v
+        self.unique = unique
+
+    def _iter_links(self, document):
+        scripts = document.xpath('//script[@type="text/javascript"]/text()').extract()
+        array_text = None
+        for script in scripts:
+            if script.startswith('window.INIT_data'):
+                array_text = script.replace(u'\\u003d', u'=')
+                # TODO: evaluate javascript properly
+                break
+        if array_text is None:
+            return []
+        else:
+            all_urls = []
+            for regexp in self.url_regexps:
+                all_urls.extend(re.findall(regexp, array_text))
+            print all_urls[:10]
+            return all_urls
+
+    def _extract_links(self, selector, response_url, response_encoding, base_url):
+        links = []
+        # hacky way to get the underlying lxml parsed document
+        for attr_val in self._iter_links(selector):
+            # pseudo lxml.html.HtmlElement.make_links_absolute(base_url)
+            try:
+                attr_val = urljoin(base_url, attr_val)
+            except ValueError:
+                continue  # skipping bogus links
+            else:
+                url = self.process_attr(attr_val)
+                if url is None:
+                    continue
+            url = to_native_str(url, encoding=response_encoding)
+            # to fix relative links after process_value
+            url = urljoin(response_url, url)
+            link = Link(url, u'', nofollow=False)
+            links.append(link)
+        return self._deduplicate_if_needed(links)
+
+
+class GoogleApiLinkExtractor(scrapy.linkextractors.FilteringLinkExtractor):
+    def __init__(self, allow=(), deny=(), allow_domains=(), deny_domains=(), restrict_xpaths=(),
+                 canonicalize=True, unique=True, process_value=None, deny_extensions=None, restrict_css=(),
+                 base_url=None):
+        """ `allow` specifies which regexps to use to extract links """
+        allow_res = [x if isinstance(x, _re_type) else re.compile(x) for x in arg_to_iter(allow)]
+
+        lx = ParserLinkExtractor(allow_res, unique=unique, process=process_value)
+
+        super(GoogleApiLinkExtractor, self).__init__(lx, allow=allow_res, deny=deny,
+            allow_domains=allow_domains, deny_domains=deny_domains,
+            restrict_xpaths=restrict_xpaths, restrict_css=restrict_css,
+            canonicalize=canonicalize, deny_extensions=deny_extensions)
+        self.base_url = base_url
+
+    def extract_links(self, response):
+        base_url = self.base_url if self.base_url else get_base_url(response)
+        if self.restrict_xpaths:
+            docs = [subdoc
+                    for x in self.restrict_xpaths
+                    for subdoc in response.xpath(x)]
+        else:
+            docs = [response.selector]
+        all_links = []
+        for doc in docs:
+            links = self._extract_links(doc, response.url, response.encoding, base_url)
+            print 'Num links:', len(links)
+            all_links.extend(self._process_links(links))
+        print 'Num links:', len(all_links)
+        return unique_list(all_links)
--- googleart_scraper/spiders/crawler.py
+++ googleart_scraper/spiders/crawler.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
+from scrapy import Request
+
 from googleart_scraper.items import ArtworkItem
+from googleart_scraper.linkextractor import GoogleApiLinkExtractor
 
 
 # TODO: on teh page of artist - get a painting url from a scroll list
@@ -11,30 +13,47 @@ from googleart_scraper.items import ArtworkItem
 
 class GoogleartCrawlSpider(CrawlSpider):
     name = 'artuk_crawler'
-    allowed_domains = ['stackoverflow.com']
-    start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
+    # allowed_domains = ['https://www.google.com/']
+    start_urls = ['https://www.google.com/culturalinstitute/beta/category/artist']
 
     rules = [
-        Rule(LinkExtractor(allow=r'questions\?page=[0-9]&sort=newest'),
-             callback='parse_item', follow=True)
+        Rule(GoogleApiLinkExtractor(base_url=u'https://www.google.com/culturalinstitute/beta/',
+                                    allow=u'entity/.*\?categoryId\u003dartist'),
+             callback='parse_artist', follow=True),
+        Rule(GoogleApiLinkExtractor(base_url=u'https://www.google.com/culturalinstitute/beta/',
+                                    allow=u'asset/.+/.+'),
+             callback='parse_artwork', follow=False)
     ]
 
-    def parse(self, response):
-        questions = response.xpath('//div[@class="summary"]/h3')
+    # def parse(self, response):
+    #     questions = response.xpath('//div[@class="summary"]/h3')
+    #
+    #     for question in questions:
+    #         item = ArtworkItem()
+    #         item['url'] = question.xpath(
+    #             'a[@class="question-hyperlink"]/@href').extract()[0]
+    #         item['title'] = question.xpath(
+    #             'a[@class="question-hyperlink"]/text()').extract()[0]
+    #         yield item
 
-        for question in questions:
-            item = ArtworkItem()
-            item['url'] = question.xpath(
-                'a[@class="question-hyperlink"]/@href').extract()[0]
-            item['title'] = question.xpath(
-                'a[@class="question-hyperlink"]/text()').extract()[0]
-            yield item
+    def parse_artist(self, response):
+        self.logger.info('A response from %s just arrived!', response.url)
+        # TODO:
+        name = response.xpath('//*[@id="yDmH0d"]/div[3]/header/div[2]/h1/text()').extract()
+        print 'Artist name:', name
+        pass
 
-    def parse(self, response):
-        item = response.xpath('//meta[@property="og:image"]/@content').extract_first()
-        # 'https://lh5.ggpht.com/-HvgLbwqJ7Yy1iF9imtgsGhVDBDafmvTnDRZSCKl_PNjMT_KXaoNLuO4A7tb4Q'
-        yield Request(item, self.parse_image)
+    def parse_artwork(self, response):
+        self.logger.info('A response from %s just arrived!', response.url)
+        # TODO:
+        print 'Artwork page:', response.xpath('/html/head/title/@text')
+        pass
 
-    def parse_image(self, response):
-        with open('image.jpg', 'w') as f:
-            f.write(response.body)
+    # def parse_page_wtih_img(self, response):
+    #     item = response.xpath('//meta[@property="og:image"]/@content').extract_first()
+    #     # 'https://lh5.ggpht.com/-HvgLbwqJ7Yy1iF9imtgsGhVDBDafmvTnDRZSCKl_PNjMT_KXaoNLuO4A7tb4Q'
+    #     yield Request(item, self.parse_image)
+    #
+    # def parse_image(self, response):
+    #     with open('image.jpg', 'w') as f:
+    #         f.write(response.body)
